1fc682280ea86ad862ef87d5df4199c1102b9d4e
Muhammad Al Farizzi <muhammadalfarizzi563@gmail.com>
2020-11-16 13:40:50 +0700

    third commit

--- controllers/presiden.js
+++ controllers/presiden.js
@@ -3,19 +3,20 @@ const $ = require("cheerio");
 
 
 const indonesia = (req,res) => {
-    const link = `https://id.wikipedia.org/wiki/Daftar_presiden_Indonesia`;
-    const data = [];
-    rp(link)
-    .then(html => {
-        for (let i = 2; i < $('b > a',html).length - 9; i++) {
-            data.push({
-                name: $('b > a',html)[i].attribs.title,
-                link : `https://id.wikipedia.org${$('b > a',html)[i].attribs.href}`
-            })            
-        }
-        res.json(data);
-    })
-    .catch(e => {throw e});
+    res.send('bisa');
+    // const link = `https://id.wikipedia.org/wiki/Daftar_presiden_Indonesia`;
+    // const data = [];
+    // rp(link)
+    // .then(html => {
+    //     for (let i = 2; i < $('b > a',html).length - 9; i++) {
+    //         data.push({
+    //             name: $('b > a',html)[i].attribs.title,
+    //             link : `https://id.wikipedia.org${$('b > a',html)[i].attribs.href}`
+    //         })            
+    //     }
+    //     res.json(data);
+    // })
+    // .catch(e => {throw e});
 }
 
 module.exports = {
