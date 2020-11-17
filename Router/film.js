f313b75cecf39bd87923189e2c6d9823b089d56c
Muhammad Al Farizzi <muhammadalfarizzi563@gmail.com>
2020-11-17 12:19:51 +0700

    added the latest comedy film scraping data

--- Router/film.js
+++ Router/film.js
@@ -3,5 +3,6 @@ const film = require("../controllers/film");
 
     Router.get('/latest', film.latest);
     Router.get('/action', film.action);
+    Router.get('/comedy', film.comedy);
 
 module.exports = Router;
--- controllers/film.js
+++ controllers/film.js
@@ -34,12 +34,31 @@ const action = (req,res) => {
             data[i] = {...data[i], rating: rating[i].next?.data || "-", durasi: durasi[i].next?.data || "-"}
         }
         res.json(data);
-        res.end();
+    })
+    .catch(e => {throw e})
+}
+const comedy = (req,res) => {
+    const link = `http://85.114.138.56/genre/comedy/`;
+    let data = [];
+    rp(link)
+    .then(html => {
+        let length = $('.mli-info > h2',html);
+        let durasi = $('.rating-durasi > span.mli-durasi > i',html);
+        let rating = $('.rating-durasi > span.mli-rating > i',html);
+        for (let i = 0; i < length.length; i++) {
+            data.push({title: length[i].children[0].data});
+        }
+        for (let i = 0; i < length.length; i++) {
+            data[i] = {...data[i], rating: rating[i].next?.data || "-", durasi: durasi[i].next?.data || "-"}
+        }
+        res.json(data);
     })
     .catch(e => {throw e})
 }
 
 module.exports = {
     latest: latest,
-    action: action
+    action: action,
+    comedy: comedy,
+
 }
